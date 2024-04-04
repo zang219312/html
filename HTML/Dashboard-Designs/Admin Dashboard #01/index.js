d8bcc1697be16ae30ae461432b6904c217fc0776
zhangding <2324674973@qq.com>
2024-04-04 15:40:27 +0800

    fix:添加模板

---
+++ HTML/Dashboard-Designs/Admin Dashboard #01/index.js
@@ -0,0 +1,36 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    // sideMenu.style.display = 'block';
+    sideMenu.className = 'show-aside'
+});
+
+closeBtn.addEventListener('click', () => {
+    // sideMenu.style.display = 'none';
+    sideMenu.className = 'hidden-aside'
+
+});
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
+        <td class="primary">Details</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+});
